7ad410216edb4c39f8026b4faf968f88d56d9e9f
zzangoobrother <com1368@naver.com>
2022-01-19 21:25:03 +0900

    [feat] 특정 회원만 이용가능한 url 학습

--- src/main/java/com/example/securitystudyclub/config/SecurityConfig.java
+++ src/main/java/com/example/securitystudyclub/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.securitystudyclub.security.handler.ClubLoginSuccessHandler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Bean
@@ -28,9 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .antMatchers("/sample/all").permitAll()
-        .antMatchers("/sample/member").hasRole("USER");
+//    http.authorizeRequests()
+//        .antMatchers("/sample/all").permitAll()
+//        .antMatchers("/sample/member").hasRole("USER");
 
     http.formLogin();
     http.csrf().disable();
--- src/main/java/com/example/securitystudyclub/controller/SampleController.java
+++ src/main/java/com/example/securitystudyclub/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.example.securitystudyclub.controller;
 
 import com.example.securitystudyclub.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample/")
 public class SampleController {
 
+  @PreAuthorize("permitAll()")
   @GetMapping("/all")
   public void exAll() {
     log.info("exAll..................");
   }
 
+  @PreAuthorize("hasRole('USER')")
   @GetMapping("/member")
   public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
     log.info("exMember..................");
@@ -25,8 +28,18 @@ public class SampleController {
     log.info(clubAuthMemberDTO);
   }
 
+  @PreAuthorize("hasRole('ADMIN')")
   @GetMapping("/admin")
   public void exAdmin() {
     log.info("exAdmin..................");
+  }
+
+  @PreAuthorize("#clubAuthMemberDTO != null && #clubAuthMemberDTO.username eq \"user95@test.org\"")
+  @GetMapping("/exOnly")
+  public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
+    log.info("exMemberOnly........................");
+    log.info(clubAuthMemberDTO);
+
+    return "/sample/admin";
   }
 }
